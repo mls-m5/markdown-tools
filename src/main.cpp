570cf8550849b66a4db8634338dace791f511a2e
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-03-28 11:22:00 +0200

    First working version

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <regex>
 #include <string>
 #include <vector>
 
@@ -8,6 +10,7 @@ auto helpStr = R"_(
 usage:
 -i                     where to put images
 -p                     prefix to image filenames
+--dry-run              dont do anything, just print result
 )_";
 
 void printHelp() {
@@ -20,6 +23,7 @@ struct Settings {
     path markdownPath;
     path imagePath;
     std::string prefix;
+    bool shouldDryRun = false;
 
     Settings(int argc, char **argv) {
         auto args = std::vector<std::string>(argv + 1, argv + argc);
@@ -30,8 +34,11 @@ struct Settings {
             if (arg == "-i") {
                 imagePath = next();
             }
-            if (arg == "-p") {
+            else if (arg == "-p") {
                 prefix = next();
+            }
+            else if (arg == "--dry-run") {
+                shouldDryRun = true;
             }
             else {
                 markdownPath = arg;
@@ -46,8 +53,83 @@ struct Settings {
     }
 };
 
+void download(std::string url, std::filesystem::path path) {
+    std::cerr << "downloading to " << path << " from " << url << std::endl;
+
+    auto ss = std::ostringstream{};
+    ss << "wget " << url << " -O " << path;
+
+    if (std::system(ss.str().c_str())) {
+        std::cerr << "failed to download " << url << "\n";
+        std::terminate();
+    }
+}
+
 int main(int argc, char **argv) {
     const auto settings = Settings{argc, argv};
 
-    std::cout << settings.imagePath << std::endl;
+    auto file = std::ifstream{settings.markdownPath};
+
+    if (!file) {
+        std::cerr << "could not open markdown file\n";
+        std::terminate();
+    }
+
+    // Match link [...](...)
+    auto re = std::regex{"!\\[(.*)\\]\\((.*)\\)"};
+
+    auto filenameBase = settings.prefix + "-image";
+    if (filenameBase.front() == '-') {
+        filenameBase.erase(0, 1);
+    }
+
+    auto imageNum = size_t{0};
+
+    auto os = std::ostringstream{};
+
+    for (std::string line; std::getline(file, line);) {
+        if (std::regex_match(line, re)) {
+            auto count = size_t{0};
+            auto replacementName = std::string{};
+            for (auto it = std::sregex_iterator{line.begin(), line.end(), re};
+                 it != std::sregex_iterator{};
+                 ++it) {
+                auto match = *it;
+                ++count;
+
+                // Dont know, maybe its not always pngs
+                replacementName =
+                    filenameBase + "-" + std::to_string(imageNum) + ".png";
+
+                if (!settings.shouldDryRun) {
+                    download(match[2], settings.imagePath / replacementName);
+                }
+            }
+
+            if (count > 1) {
+                std::cerr << "line " << line << ":\n";
+                std::cerr << "multiple images per line is not supported\n";
+                std::terminate();
+            }
+
+            os << std::regex_replace(line, re, "![[" + replacementName + "]]")
+               << "\n";
+            ++imageNum;
+        }
+
+        else {
+            os << line << "\n";
+        }
+    }
+
+    file.close();
+
+    if (!settings.shouldDryRun) {
+        auto outfile = std::ofstream{settings.markdownPath};
+
+        outfile << os.str();
+    }
+    else {
+        std::cout << os.str();
+    }
 }
